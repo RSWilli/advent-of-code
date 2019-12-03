fdb8bee431452cc760a22240aa1a6983c1047d26
RSWilli <bartel.wilhelm@gmail.com>
2019-12-03 20:55:09 +0100

    day 3 part 1

--- 2019/3/part1/main.hs
+++ 2019/3/part1/main.hs
@@ -3,80 +3,56 @@
 import           System.IO
 import           Data.List.Split
 import           Data.List
-import           Control.Monad                  ( guard )
-import qualified Data.Map.Strict               as M
+import qualified Data.Set               as S
 
-data Direction = U | L | D | R deriving (Show)
-type RelativePath = [(Direction, Int)]
-
+type Field = S.Set Position
 type Position = (Int, Int)
-type Way = (Position, Position)
-type AbsolutePath = [Position]
 
-parseFile :: String -> IO [RelativePath]
+getDestination :: Position -> String -> Position
+getDestination (x,y) step = case step of
+    'U':distance -> (x, y + read distance)
+    'D':distance -> (x, y - read distance)
+    'L':distance -> (x - read distance, y)
+    'R':distance -> (x + read distance, y)
+
+parseFile :: String -> IO [Field]
 parseFile path = do
     handle   <- openFile path ReadMode
     contents <- hGetContents handle
     let pathStrings = lines contents
     let paths       = map (splitOn ",") pathStrings :: [[String]]
+    print paths
 
-    let path = map
-            (map
-                (\(x : xs) ->
-                    let convert = \case
-                            'U' -> U
-                            'L' -> L
-                            'D' -> D
-                            'R' -> R
-                            x   -> error "unknown direction"
-                    in  (convert x, read xs :: Int)
+    let fields = map (snd.foldl 
+                (\(pos,set) step ->
+                    let destination = getDestination pos step
+                        steps = getSteps pos destination
+                        newset = foldl (flip S.insert) set steps
+                        in
+                            (destination, newset)
                 )
+                ((0,0), S.empty)
             )
             paths
-    return path
+    return fields
 
-testFile :: IO [RelativePath]
+testFile :: IO [Field]
 testFile = parseFile "./test.txt"
 
-inputFile :: IO [RelativePath]
+inputFile :: IO [Field]
 inputFile = parseFile "../input.txt"
 
-createAbsolutePath :: RelativePath -> AbsolutePath
-createAbsolutePath = reverse . foldl
-    (\path@((x, y) : _) (direction, distance) ->
-        let newpos = case direction of
-                U -> (x, y + distance)
-                D -> (x, y - distance)
-                L -> (x - distance, y)
-                R -> (x + distance, y)
-        in  newpos : path
-    )
-    [(0, 0)]
-
-normalizeWay :: Way -> Way
-normalizeWay (p1, p2) = case compare p1 p2 of
-        LT -> (p1, p2)
-        GT -> (p2, p1)
-        EQ -> error "no way"
-
-getWays :: AbsolutePath -> [Way]
-getWays (start : second : rest) = reverse $ map normalizeWay $ foldl
-    (\ways@((_, (curX, curY)) : _) pos -> ((curX, curY), pos) : ways)
-    [(start, second)]
-    rest
-
-getPointsOnWay :: Way -> [Position]
-getPointsOnWay ((x1, y1), (x2, y2)) = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
-
--- getIntersections :: [Way] -> [Way] -> [Position]
-getIntersections w1 w2 = let {
-            pointsW1 = concatMap getPointsOnWay w1 ;
-            pointsW2 = concatMap getPointsOnWay w2
-    } in pointsW1 `intersect` pointsW2
-
+getSteps :: Position -> Position -> [Position]
+getSteps x y = 
+    let ((x1,x2), (y1,y2)) = case compare x y of
+            LT -> (x,y)
+            GT -> (y,x)
+            EQ -> error "no way"
+        in
+            [(x,y) | x <- [x1..y1], y <- [x2..y2]]
+    
 main = do
-    paths <- testFile
-    let (p1:p2:_) = map (getWays . createAbsolutePath) paths
-    let intersections = getIntersections p1 p2
-    let distances = sort $ map (uncurry (+)) intersections
+    p1:p2:_ <- inputFile
+    let intersections = p1 `S.intersection` p2
+    let distances = S.map (\(x,y) -> abs x + abs y) intersections
     print distances
