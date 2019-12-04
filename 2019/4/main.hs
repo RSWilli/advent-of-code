eb96e31d6f9033d9e2181071ffd3f64a339a0577
RSWilli <bartel.wilhelm@gmail.com>
2019-12-04 18:11:35 +0100

    day 4 part 2

--- 2019/4/main.hs
+++ 2019/4/main.hs
@@ -1,20 +1,38 @@
-range = [197487..673251] :: [Int]
+import Data.Time.Clock.System
+
+range = [197487 .. 673251] :: [Int]
 
 digs :: Int -> [Int]
 digs 0 = []
 digs x = digs (x `div` 10) ++ [x `mod` 10]
 
 doubleDigits :: [Int] -> Bool
-doubleDigits [] = False
-doubleDigits [x] = False 
-doubleDigits (x:xs) = x == head xs || doubleDigits xs
+doubleDigits []       = False
+doubleDigits [x     ] = False
+doubleDigits (x : xs) = x == head xs || doubleDigits xs
 
 monotone :: [Int] -> Bool
-monotone [] = True
-monotone [x] = True
-monotone (x:xs) = x <= head xs && monotone xs
+monotone []       = True
+monotone [x     ] = True
+monotone (x : xs) = x <= head xs && monotone xs
+
+exactlyOnePair :: [Int] -> Bool
+exactlyOnePair []  = False
+exactlyOnePair [x] = False
+exactlyOnePair xs =
+    let x = head xs
+        eq = takeWhile (x ==) xs
+        neq = dropWhile (x ==) xs
+        in
+            length eq == 2 || exactlyOnePair neq
 
 main = do
+    start <- getSystemTime
     let digits = map digs range
     let part1 = filter (\v -> monotone v && doubleDigits v) digits
+    let part2 = filter (\v -> monotone v && exactlyOnePair v) digits
     putStrLn $ "part1: " ++ show (length part1)
+    putStrLn $ "part2: " ++ show (length part2)
+    end <- getSystemTime
+
+    print $ systemNanoseconds end - systemNanoseconds start
