39642f4fdd5c1e1f2564da96f9b898f77ae095a8
RSWilli <bartel.wilhelm@gmail.com>
2019-12-07 19:27:38 +0100

    reformat

--- 2019/6/part2/main.hs
+++ 2019/6/part2/main.hs
@@ -1,36 +1,38 @@
-import Data.List.Split
-import System.IO
-import qualified Data.Map as M
-import Control.Applicative
+import           Data.List.Split
+import           System.IO
+import qualified Data.Map                      as M
+import           Control.Applicative
 
 type Orbits = M.Map String [String]
 
 parseInput :: String -> Orbits
-parseInput str = 
+parseInput str =
     let ls = lines str
         kv = map (splitOn ")") ls
-        in M.fromListWith (++) $ concatMap 
-            (\(x:y:_) -> [(x,[y]), (y, [x])])
-            kv
+    in  M.fromListWith (++)
+            $ concatMap (\(x : y : _) -> [(x, [y]), (y, [x])]) kv
 
 findPath :: String -> String -> Orbits -> [String] -> Maybe [String]
-findPath current destination graph visited = 
-    let newVisited = current:visited
-        in
-            if current == destination then return newVisited else do
+findPath current destination graph visited =
+    let newVisited = current : visited
+    in
+        if current == destination
+            then return newVisited
+            else do
                 connectedNodes <- M.lookup current graph
                 let next = filter (`notElem` visited) connectedNodes
-                let 
-                let paths = map (\v -> findPath v destination graph newVisited) next
+                let
+                let paths =
+                        map (\v -> findPath v destination graph newVisited) next
 
                 foldr (liftA2 (++)) (return []) paths
 
 
 main = do
-        handle <- openFile "../input.txt" ReadMode  
-        contents <- hGetContents handle
-        let orbits = parseInput contents
-        print orbits
-        let path = findPath "YOU" "SAN" orbits []
-        print path
-        print $ (subtract 3).length <$> path
+    handle   <- openFile "../input.txt" ReadMode
+    contents <- hGetContents handle
+    let orbits = parseInput contents
+    print orbits
+    let path = findPath "YOU" "SAN" orbits []
+    print path
+    print $ (subtract 3) . length <$> path
