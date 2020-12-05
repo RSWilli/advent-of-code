95295e7cf5cb41397e7b787695f1c8b1e50fbbda
RSWilli <bartel.wilhelm@gmail.com>
2020-12-05 19:45:00 +0100

    using xor for day 5 part 2

--- 2020/days/day05.hs
+++ 2020/days/day05.hs
@@ -2,6 +2,7 @@
 
 import Bench
 import Control.Applicative
+import Data.Bits (xor)
 import qualified Data.Set as S
 import InputParser
 import Util
@@ -12,14 +13,16 @@ seatParser = foldl (\sum i -> sum * 2 + i) 0 <$> many (1 <$ ("B" <|> "R") <|> (0
 part1 :: [Int] -> Int
 part1 = maximum
 
-part2 :: [Int] -> S.Set Int
+part2 :: [Int] -> Int
 part2 passes =
   let seats = S.fromList passes
-      possibleSeats = S.fromDistinctAscList [S.findMin seats .. S.findMax seats]
-   in possibleSeats S.\\ seats
+   in foldr xor 0 [0 .. (S.findMin seats - 1)]
+        `xor` S.foldr xor 0 seats
+        `xor` foldr xor 0 [(S.findMax seats + 1) .. 1023]
 
 main = do
   passes <- parseInputLines 5 seatParser
+  print passes
   print $ part1 passes
   print $ part2 passes
   defaultMain
