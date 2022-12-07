a5fb51f9378155cc1834def633b8715e8ecb1f0e
RSWilli <bartel.wilhelm@gmail.com>
2022-12-07 22:11:38 +0100

    day7 part1 and part2
    * thanks @hashworks

--- 2022/rust/days/07/src/main.rs
+++ 2022/rust/days/07/src/main.rs
@@ -1,33 +1,102 @@
-use filesystem::FileSystem;
+use std::{collections::HashMap, path::PathBuf};
+
 use lib::{AOCError, AOCReader, AdventOfCode};
-use terminalOutput::TerminalOutput;
 
 struct Day {}
 
-mod filesystem;
+fn sum_directories(files: &[(String, usize)]) -> HashMap<String, usize> {
+    let mut tree = HashMap::new();
 
-mod terminalOutput;
+    for (path, size) in files {
+        let pathb = PathBuf::from(path);
+        for ancestor in pathb.parent().unwrap().ancestors() {
+            let dirsize = tree
+                .entry(ancestor.to_string_lossy().to_string())
+                .or_insert(0);
+
+            *dirsize += size
+        }
+    }
+
+    tree
+}
 
 impl AdventOfCode for Day {
     const DAY: usize = 7;
 
-    type In = FileSystem;
+    type In = Vec<(String, usize)>;
 
     type Out = usize;
 
     fn parse(&self, inp: AOCReader) -> Result<Self::In, AOCError> {
-        let output: Result<Vec<TerminalOutput>, AOCError> = inp.parse_lines().collect();
+        let mut files = Vec::new();
+        let mut currentpath = PathBuf::from("/");
 
-        output?.try_into()
+        for line in inp.lines().skip(2) {
+            let line = line?;
+
+            if let Some(cmd) = line.strip_prefix("$ ") {
+                match cmd {
+                    "ls" => continue,
+                    _ => {
+                        if let Some(target) = cmd.strip_prefix("cd ") {
+                            match target {
+                                ".." => {
+                                    currentpath = currentpath
+                                        .parent()
+                                        .ok_or(AOCError::ParseErr())?
+                                        .to_path_buf();
+                                }
+                                target => currentpath = currentpath.join(target),
+                            }
+                        } else {
+                            return Err(AOCError::ParseErr());
+                        }
+                    }
+                }
+            } else if let Some((dir_or_size, name)) = line.split_once(' ') {
+                match dir_or_size {
+                    "dir" => {}
+                    size => {
+                        let size = size.parse()?;
+
+                        let path = currentpath.join(name).to_string_lossy().to_string();
+
+                        files.push((path, size));
+                    }
+                }
+            } else {
+                return Err(AOCError::ParseErr());
+            }
+        }
+
+        Ok(files)
     }
 
     fn part1(&self, input: &Self::In) -> Result<Self::Out, AOCError> {
-        unimplemented!()
-        // Ok(input.iter().sum())
+        let dir_sizes = sum_directories(input);
+
+        Ok(dir_sizes
+            .into_iter()
+            .filter(|(_, size)| size <= &100000)
+            .map(|(_, s)| s)
+            .sum())
     }
 
     fn part2(&self, input: &Self::In) -> Result<Self::Out, AOCError> {
-        unimplemented!()
+        let dir_sizes = sum_directories(input);
+
+        let root_size = dir_sizes.get("/").ok_or(AOCError::AOCError {
+            msg: "no root found",
+        })?;
+
+        let space_needed = *root_size - (70000000 - 30000000);
+
+        Ok(*dir_sizes
+            .values()
+            .filter(|x| x > &&space_needed)
+            .min()
+            .unwrap_or(&0))
     }
 }
 
@@ -41,6 +110,11 @@ mod tests {
 
     #[test]
     fn test1() -> Result<(), AOCError> {
-        lib::test(Day {}, lib::Part::Part1, 1, 10)
+        lib::test(Day {}, lib::Part::Part1, 1, 95437)
+    }
+
+    #[test]
+    fn test2() -> Result<(), AOCError> {
+        lib::test(Day {}, lib::Part::Part2, 1, 24933642)
     }
 }
