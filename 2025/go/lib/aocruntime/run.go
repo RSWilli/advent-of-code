1974227f5ae66b653eeae214e5e18a46b0468382
RSWilli <bartel.wilhelm@gmail.com>
2025-12-04 22:21:34 +0100

    better running with panic recover and benchmarking

--- 2025/go/lib/aocruntime/run.go
+++ 2025/go/lib/aocruntime/run.go
@@ -1,6 +1,9 @@
 package aocruntime
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type InputFunc func() string
 type AOCFunc func(string) string
@@ -8,6 +11,21 @@ type AOCFunc func(string) string
 func RunDay(in InputFunc, p1, p2 AOCFunc) {
 	input := in()
 
-	fmt.Printf("Part1: %s\n", p1(input))
-	fmt.Printf("Part2: %s\n", p2(input))
+	runPart(1, input, p1)
+	runPart(2, input, p2)
+}
+
+func runPart(p int, in string, part AOCFunc) {
+	defer func() {
+		// print panic
+		if r := recover(); r != nil {
+			fmt.Printf("Part %d: panic: %v\n", p, r)
+		}
+	}()
+
+	now := time.Now()
+	result := part(in)
+	elapsed := time.Since(now)
+
+	fmt.Printf("Part %d: %s (took %s)\n", p, result, elapsed)
 }
